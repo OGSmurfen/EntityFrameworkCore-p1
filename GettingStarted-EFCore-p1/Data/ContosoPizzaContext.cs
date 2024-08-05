8fe5bc0d72cb4312709bf41b9e5c25622dcd752e
Ilia Chakarov <114438460+OGSmurfen@users.noreply.github.com>
2024-08-05 20:21:09 +0300

    Add project files.

---
+++ GettingStarted-EFCore-p1/Data/ContosoPizzaContext.cs
@@ -0,0 +1,23 @@
+﻿using GettingStarted_EFCore_p1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted_EFCore_p1.Data
+{
+    public class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=ContosoPizza-Part1;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ GettingStarted-EFCore-p1/Program.cs
@@ -0,0 +1,62 @@
+﻿using GettingStarted_EFCore_p1.Data;
+using GettingStarted_EFCore_p1.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+//Add products to DB
+//Product veggieSpecial = new Product()
+//{
+//    Name = "Veggie Special",
+//    Price = 9.99M
+//};
+//context.Products.Add(veggieSpecial);
+
+//Product margaritaPizza = new Product()
+//{
+//    Name = "Margarita Pizza",
+//    Price = 5.00M
+//};
+//context.Products.Add(margaritaPizza);
+//Product fungi = new Product()
+//{
+//    Name = "Pizza Fungi",
+//    Price = 15M
+//};
+//context.Add(fungi);
+//context.SaveChanges();
+
+
+//Retrieve and change price
+//var veggieSpecialRetrieved = context.Products.Where(p => p.Name == "Veggie Special").FirstOrDefault();
+//if(veggieSpecialRetrieved is Product)
+//{
+//    veggieSpecialRetrieved.Price = 20M;
+//}
+//context.SaveChanges();
+
+// Delete entity:
+var veggieSpecialToDelete = context.Products
+            .Where(_product => _product.Name == "Veggie Special")
+            .FirstOrDefault();
+if (veggieSpecialToDelete is Product)
+{
+    context.Remove(veggieSpecialToDelete);
+}
+context.SaveChanges();
+
+
+
+
+
+//read from db:
+var products = context.Products
+    .Where(p => p.Price > 10M)
+    .OrderBy(p => p.Name);//fluent api syntax
+
+foreach(Product p in products)
+{
+    Console.WriteLine($"Id: {p.Id}");
+    Console.WriteLine($"Name: {p.Name}");
+    Console.WriteLine($"Price: {p.Price}");
+    Console.WriteLine(new string('-', 20));
+}
